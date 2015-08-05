e6bb23bd446c0966892f899804fda71a9f12ab94
Brian Chen <bchen.chen93@gmail.com>
2015-08-06 10:52:57 +1200

    fix: Rebase to master

---
+++ js/model.js
@@ -0,0 +1,34 @@
+var model = {
+  projects: [
+    {
+      "name": "Online Resume",
+      "image": "images/frogger.png",
+      "imageAlt": "screenshot of frogger game",
+      "link": "http://brianzchen.github.io/online-resume/"
+    },
+    {
+      "name": "Classic Arcade Game",
+      "image": "images/frogger.png",
+      "imageAlt": "screenshot of frogger game",
+      "link": "http://brianzchen.github.io/frogger-game/"
+    },
+    {
+      "name": "Web Optimization",
+      "image": "images/frogger.png",
+      "imageAlt": "screenshot of frogger game",
+      "link": "http://brianzchen.github.io/web-opt-project/"
+    },
+    {
+      "name": "Neighbourhood Map",
+      "image": "images/frogger.png",
+      "imageAlt": "screenshot of frogger game",
+      "link": "http://brianzchen.github.io/east-eats/"
+    },
+    {
+      "name": "Feed Reader",
+      "image": "images/frogger.png",
+      "imageAlt": "screenshot of frogger game",
+      "link": "https://github.com/Brianzchen/feedreader"
+    }
+  ]
+}
