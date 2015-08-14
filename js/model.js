c70a36414222213d4af7e579be79d82ac50fff63
Brian Chen <bchen.chen93@gmail.com>
2015-08-14 16:38:01 +1200

    feat: New project added, delivery checklist

--- js/model.js
+++ js/model.js
@@ -29,6 +29,12 @@ var model = {
       "image": "images/feedreader.png",
       "imageAlt": "jasmine tests showing no errors",
       "link": "https://github.com/Brianzchen/feedreader"
+    },
+    {
+      "name": "Delivery Checklist",
+      "image": "images/delivery-checklist.png",
+      "imageAlt": "master order form",
+      "link": "http://brianzchen.github.io/delivery-checklist/"
     }
   ]
 }
